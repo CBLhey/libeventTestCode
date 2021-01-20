b76fc9a450cba5fb01ea708b2c4a97892e97b083
vicent <1119395215@qq.com>
2021-01-20 10:39:27 +0000

    test

--- libeventTest003/libeventTest003.cpp
+++ libeventTest003/libeventTest003.cpp
@@ -20,10 +20,63 @@ int main(){
     }
 #endif
 
-    //初始话libevent上下文
+    //创建libevent上下文
+    event_config *conf = event_config_new();
 
-    //配置上下文
+    //显示支持的网络模式
+    const char **methods = event_get_supported_methods();
+    cout << "supported_methods: " << endl;
+    for (int i=0; methods[i] != NULL; i++){
+        cout << methods[i] << endl; 
+    }
 
+    //设置特征
+    //event_config_require_features(conf, EV_FEATURE_ET);
+
+    //配置libevent上下文
+    event_base *base = event_base_new_with_config(conf);
+    event_config_free(conf);
+
+    if (!base){
+        cerr << "event_base_new_with_config failed!" << endl;
+    }
+    else{
+
+        //确认特征是否生效
+        int f = event_base_get_features(base);
+        if (f&EV_FEATURE_ET){
+            cout << "EV_FEATURE_ET events are supported." << endl;
+        }
+        else{
+            cout << "EV_FEATURE_ET events are not supported." << endl;
+        }
+
+        if (f&EV_FEATURE_O1){
+            cout << "EV_FEATURE_O1 events are supported." << endl;
+        }
+        else{
+            cout << "EV_FEATURE_O1 events are not supported." << endl;
+        }
+
+        if (f&EV_FEATURE_FDS){
+            cout << "EV_FEATURE_FDS events are supported." << endl;
+        }
+        else{
+            cout << "EV_FEATURE_FDS events are not supported." << endl;
+        }
+
+        if (f&EV_FEATURE_EARLY_CLOSE){
+            cout << "EV_FEATURE_EARLY_CLOSE events are supported." << endl;
+        }
+        else{
+            cout << "EV_FEATURE_EARLY_CLOSE events are not supported." << endl;
+        }
+
+        cout << "event base_new_with_config success!" << endl;
+        event_base_free(base);
+    }
+
+    event_base_new_with_config(conf);
    
 #ifdef _WIN32
     WSACleanup();
