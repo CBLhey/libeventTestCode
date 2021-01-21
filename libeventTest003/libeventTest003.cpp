86071d4b5ebadc6cb54bfd4da4bf98d2d274fbe2
vicent <1119395215@qq.com>
2021-01-21 08:37:52 +0000

    test

--- libeventTest003/libeventTest003.cpp
+++ libeventTest003/libeventTest003.cpp
@@ -1,11 +1,20 @@
 #include <event2/event.h>
+#include <event2/thread.h>
+#include <event2/listener.h> 
 #include <iostream>
-#ifdef _WIN32
-#else
+#include <string.h>
+#ifndef _WIN32
     #include <signal.h>
 #endif
 
 using namespace std;
+
+#define SOPRT 5001
+
+void listen_cb(evconnlistener *ev, evutil_socket_t s, struct sockaddr *addr, int socklen, void *agr){
+    cout << "lesson_cb" << endl;
+}
+
 
 int main(){
 
@@ -31,7 +40,22 @@ int main(){
     }
 
     //设置特征
-    //event_config_require_features(conf, EV_FEATURE_ET);
+    //event_config_require_features(conf, EV_FEATURE_FDS);
+
+    //设置网络模型，使用select
+    //event_config_avoid_method(conf, "epoll");
+    //event_config_avoid_method(conf, "poll");
+
+    //windows中支持IOCP
+#ifdef _WIN32
+    event_config_set_flag(conf EVENT_BASE_FLAG_STARTUP_IOCP);
+    //IOCP必须初始话线程
+    evthread_use_windows_threads();
+    //设置cpu数量
+    SYSTEM_INFO si;
+    GetSytemInfo(&si);
+    event_config_set_num_cpus_hint(conf, si.dwNumberOfProcessors);
+#endif
 
     //配置libevent上下文
     event_base *base = event_base_new_with_config(conf);
@@ -39,8 +63,15 @@ int main(){
 
     if (!base){
         cerr << "event_base_new_with_config failed!" << endl;
+        base = event_base_new();
+        if (!base){
+            cerr << "event_base_new failed!" << endl;
+            return 0;
+        }
     }
     else{
+        //获取当前网络模型
+        cout << "current method is " << event_base_get_method(base) << endl;
 
         //确认特征是否生效
         int f = event_base_get_features(base);
@@ -75,6 +106,23 @@ int main(){
         cout << "event base_new_with_config success!" << endl;
         event_base_free(base);
     }
+
+    /*
+    sockaddr_in sin;
+    memset(&sin, 0, sizeof(sin));
+    sin.sin_family = AF_INET;
+    sin.sin_port = htons(SOPRT);
+
+   evconnlistener *ev = evconnlistener_new_bind(base, listen_cb, base, 10,
+        LEV_OPT_CLOSE_ON_FREE|LEV_OPT_REUSEABLE,
+        (sockaddr *)&sin, sizeof(sin)
+    );
+
+    event_base_dispatch(base);
+    if (ev) {
+        evconnlistener_free(ev);
+    }
+    */
 
     event_base_new_with_config(conf);
    
